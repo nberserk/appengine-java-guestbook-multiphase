8b2af959d9c1c23a112df947081ecfd27b3d03ea
Darren Ha <darren.ha@samsung.com>
2015-02-06 11:27:25 +0900

    update desc; housekeeping

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-appengine-java-guestbook-multiphase
-===================================
+* vote app using google app engine
+my toy project using gogle app engine to remove repetitive work.
+each person can vote one time slot. data reset everyday.
+GAE is cool. it takes just 3 day to make this after the work.
 
-Goes through the process of creating an App Engine guestbook using Java and Maven in phases
--- final/src/main/java/com/example/guestbook/TableServlet.java
+++ final/src/main/java/com/example/guestbook/TableServlet.java
@@ -1,23 +1,13 @@
 package com.example.guestbook;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.repackaged.com.google.gson.Gson;
-
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
 
 public class TableServlet extends HttpServlet {	
 	private static final long serialVersionUID = -6535578691334962972L;
@@ -32,7 +22,7 @@ public class TableServlet extends HttpServlet {
 		if(date==null)
 			date = VoteServlet.getTodayDate();
 		
-		response.addHeader("Access-Control-Allow-Origin", "*");
+		//response.addHeader("Access-Control-Allow-Origin", "*");
 		response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         TimeTable tt = VoteServlet.getTimeTable(date);
